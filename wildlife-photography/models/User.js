031a29bdc7ba01066fe0f318c737e6c32d455a94
Iliyan Rusev <ilianrusev@gmail.com>
2022-04-13 13:10:04 +0300

    basic user model

---
+++ wildlife-photography/models/User.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    hashedPassword: { type: String, required: true }
+});
+
+userSchema.index({ username: 1 }, {
+    unique: true,
+    collation: {
+        locale: 'en',
+        strength: 2
+    }
+});
+
+const User = model('User',userSchema);
+
+module.exports = User;
---
+++ wildlife-photography/services/user.js
@@ -0,0 +1,49 @@
+const User = require('../models/User');
+const { compare, hash } = require('bcrypt');
+
+async function register(username, password) {
+    const existing = await getUserByUsername(username);
+
+    if (existing) {
+        throw new Error('Already registered');
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const user = new User({
+        username,
+        hashedPassword
+    });
+
+    await user.save();
+
+    return user;
+}
+
+async function login(username, password) {
+    const user = await getUserByUsername(username);
+
+    if (!user) {
+        throw new Error('User doesn\'t exist');
+    }
+
+    const hasMatch = compare(password, user.hashedPassword);
+
+    if (!hasMatch) {
+        throw new Error('Password mismatch');
+    }
+
+    return user;
+}
+
+async function getUserByUsername(username) {
+    const user = User.find({ username })
+
+    return user;
+}
+
+
+module.exports = {
+    login,
+    register
+}
